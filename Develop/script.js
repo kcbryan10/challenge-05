b7599ddc2c0a2ab401cf30b0685f1228f3d27f18
kcbryan <kcbryan10@gmail.com>
2021-10-24 14:10:05 -0500

    save function repaired/ highlighting function started

--- Develop/script.js
+++ Develop/script.js
@@ -7,20 +7,49 @@ $(document).ready(function(){
 
     //save task to local storage
     $(".saveBtn").on("click", function () {
-        var text = $(this).siblings(".time-block").val();
+        var text = $(this).siblings(".content-input").val();
         var time = $(this).parent().attr("id");
         localStorage.setItem(time, text);
+        console.log(time);
     })
 
+    // Highlight column in relation to time
+    var timeHighlight= function(){
+        var currentTime = moment().hour();
+
+        $(".content-input").each(function(){
+            var columnTime = parseInt($(this).parent().attr("id").split("hour")[1]);
+
+            if (columnTime < currentTime){
+                $(this).removeclass("future");
+                $(this).removeclass("present");
+                $(this).addclass("past");
+            }
+
+            else if (columnTime === currentTime){
+                $(this).removeclass("future");
+                $(this).removeclass("past");
+                $(this).addclass("present");
+            }
+
+            else if(columnTime < currentTime) {
+                $(this).removeclass("past");
+                $(this).removeclass("present");
+                $(this).addclass("future");
+            }
+        })
+     }
 
     //get items from local storage and display on page 
-    $("#hr-9 .time-block").val(localStorage.getItem("09"));
-    $("#hr-10 .time-block").val(localStorage.getItem("10"));
-    $("#hr-11 .time-block").val(localStorage.getItem("11"));
-    $("#hr-12 .time-block").val(localStorage.getItem("12"));
-    $("#hr-13 .time-block").val(localStorage.getItem("13"));
-    $("#hr-14 .time-block").val(localStorage.getItem("14"));
-    $("#hr-15 .time-block").val(localStorage.getItem("15"));
-    $("#hr-16 .time-block").val(localStorage.getItem("16"));
-    $("#hr-17 .time-block").val(localStorage.getItem("17"));
+    $("#09 .content-input").val(localStorage.getItem("09"));
+    $("#10 .content-input").val(localStorage.getItem("10"));
+    $("#11 .content-input").val(localStorage.getItem("11"));
+    $("#12 .content-input").val(localStorage.getItem("12"));
+    $("#13 .content-input").val(localStorage.getItem("13"));
+    $("#14 .content-input").val(localStorage.getItem("14"));
+    $("#15 .content-input").val(localStorage.getItem("15"));
+    $("#16 .content-input").val(localStorage.getItem("16"));
+    $("#17 .content-input").val(localStorage.getItem("17"));
+
+    timeHighlight();
 });
